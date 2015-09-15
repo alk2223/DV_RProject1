b6ae05e1a11733d868d50db1342de2ad65f82cfb
Andrew Kleiman <andrewkleiman@wireless-10-147-12-115.public.utexas.edu>
2015-09-15 16:44:04 -0500

    no message

--- 02 Visualizations/Graph 1.R
+++ 02 Visualizations/Graph 1.R
@@ -1,8 +1,8 @@
 require("jsonlite")
 require("RCurl")
+require("ggplot2")
 # Change the USER and PASS below to be your UTEid
 df <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from titanic "'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_alk2223', PASS='orcl_alk2223', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
-df
 summary(df)
 head(df)
 
